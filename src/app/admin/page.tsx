c2e3c77313b0293b72091436c9096516efe49757
Руслан Сафаров <safarovr171@gmail.com>
2026-05-10 17:24:51 +0000

    Try fixing this error: `Runtime ReferenceError: Link is not defined. Err

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,6 +26,7 @@ import { getAuth, signOut, signInWithEmailAndPassword } from 'firebase/auth';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export default function AdminPage() {
   const { user, loading: authLoading } = useUser();
